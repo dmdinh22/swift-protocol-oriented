860a05c4a1b28ca4072e98361304f769f1af0c26
David Dinh <dmdinh22@gmail.com>
2019-05-04 02:41:44 -0700

    protocol dispatch

---
+++ 03-protocol-dispatch.playground/Contents.swift
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 Razeware LLC
+// See Copyright Notice page for details about the license.
+
+import Foundation
+
+protocol Distribution {
+    func sample() -> Double
+    func sample(count: Int) -> [Double]
+}
+
+// default implementation in extension
+extension Distribution {
+    func sample(count: Int) -> [Double] {
+        return (1...count).map { _ in sample()}
+    }
+}
+
+struct UniformDistribution: Distribution {
+    var range: ClosedRange<Int>
+    
+    init(range: ClosedRange<Int>) {
+        self.range = range
+    }
+    
+    func sample() -> Double {
+        return Double(Int.random(in: range))
+    }
+}
+
+let d10 = UniformDistribution(range: 1...10)
+d10.sample(count: 100)
+print(d10.sample(count: 1000).reduce(0) { $0 + ($1 == 10 ? 1 : 0)})
